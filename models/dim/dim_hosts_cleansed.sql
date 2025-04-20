c45a8a82179a0491852c4ac0634a90b1aa2b3c58
avinash k <avinn9@ymail.com>
2025-04-20 16:37:13 +0000

    .

--- models/dim/dim_hosts_cleansed.sql
+++ models/dim/dim_hosts_cleansed.sql
@@ -1,7 +1,7 @@
 with src_hosts as (select * from {{ ref("src_hosts") }})
 select
     host_id,
-    case when host_name is null then 'Anonymous' else host_name end as host_name,
+    nvl(host_name,'Anonymous') as host_name,
     is_superhost,
     created_at,
     updated_at
