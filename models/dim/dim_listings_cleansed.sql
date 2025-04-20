4d5dc01e7ebb3b72df548155b2b9cd9ffc664536
avinn9 <avinn9@ymail.com>
2025-04-20 18:27:30 +0200

    .

---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,17 @@
+with src_listings as
+(
+    select * from {{ ref('src_listings') }}
+)
+select listing_id,
+listing_name,
+listing_url,
+ROOM_TYPE,
+case when minimum_nights =1 then 1 else minimum_nights end as minimum_nights,
+host_id,
+replace(price_STR,'$','') :: NUMBER(
+    10,
+    2
+  ) AS price,
+created_at,
+updated_at
+from airbnb.dev.src_listings
