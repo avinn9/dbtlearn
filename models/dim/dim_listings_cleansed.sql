d900f21c61485771fd848c3756fb9821eb09d5fd
avinash k <avinn9@ymail.com>
2025-04-20 16:35:03 +0000

    ok

--- models/dim/dim_listings_cleansed.sql
+++ models/dim/dim_listings_cleansed.sql
@@ -1,17 +1,12 @@
-with src_listings as
-(
-    select * from {{ ref('src_listings') }}
-)
-select listing_id,
-listing_name,
-listing_url,
-ROOM_TYPE,
-case when minimum_nights =0 then 1 else minimum_nights end as minimum_nights,
-host_id,
-replace(price_STR,'$','') :: NUMBER(
-    10,
-    2
-  ) AS price,
-created_at,
-updated_at
+with src_listings as (select * from {{ ref("src_listings") }})
+select
+    listing_id,
+    listing_name,
+    listing_url,
+    room_type,
+    case when minimum_nights = 0 then 1 else minimum_nights end as minimum_nights,
+    host_id,
+    replace(price_str, '$', '')::number(10, 2) as price,
+    created_at,
+    updated_at
 from airbnb.dev.src_listings
--- models/src/src_listings.sql
+++ models/src/src_listings.sql
@@ -1,14 +1,12 @@
-WITH raw_listings AS (
-select * from airbnb.raw.raw_listings 
-)
-SELECT 
-id as listing_id,
-name as listing_name,
-listing_url,
-room_type,
-minimum_nights,
-host_id,
-price as price_str,
-created_at,
-updated_at
-FROM raw_listings
+with raw_listings as (select * from airbnb.raw.raw_listings)
+select
+    id as listing_id,
+    name as listing_name,
+    listing_url,
+    room_type,
+    minimum_nights,
+    host_id,
+    price as price_str,
+    created_at,
+    updated_at
+from raw_listings
