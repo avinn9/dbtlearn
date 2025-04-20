2292e7f664373433b06d25db6df6ee87ab1d1b35
avinn9 <avinn9@ymail.com>
2025-04-20 18:29:33 +0200

    .

--- models/dim/dim_listings_cleansed.sql
+++ models/dim/dim_listings_cleansed.sql
@@ -6,7 +6,7 @@ select listing_id,
 listing_name,
 listing_url,
 ROOM_TYPE,
-case when minimum_nights =1 then 1 else minimum_nights end as minimum_nights,
+case when minimum_nights =0 then 1 else minimum_nights end as minimum_nights,
 host_id,
 replace(price_STR,'$','') :: NUMBER(
     10,
